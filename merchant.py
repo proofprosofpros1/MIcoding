d51d00563ddc51be4574a625b9c036c07778d6ad
proofprosofpros1 <andrew.t.yoon@gmail.com>
2019-03-17 08:33:32 -0700

    Update merchant.py

--- merchant.py
+++ merchant.py
@@ -1,10 +1,19 @@
-N = int(input())
+import os
+team_path = 'C:\\temp'
+#fin = open('input.txt', 'r')
+# count = ''
+fin = open(os.path.join(team_path, 'input2.txt'), 'r')
+res = []
+N = int(fin.readline().strip())
 for i in range(N):
-    #a, p, t = map(int, input().split())
-    tmp = input().split()
-    a = int(tmp[0])
-    p = int(tmp[1])
-    t = float(tmp[2])
-    res = a * p
-    res = (res*(1-t))
-    print(res)
+   a, p, t = map(float, fin.readline().strip().split())
+   tot = a * p - a * p * t
+   res.append(str(tot))
+   # res = a * p
+   # res = (res*(1-t))
+   # count += str(res)
+   # count += '\n'
+fout = open(os.path.join(team_path, 'output2.txt'), 'w')
+# fout.write(count)
+fout.write('\n'.join(res))
+fout.close()
